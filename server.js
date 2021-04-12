53575dc94c3825360ad802fe139773c8c5959d96
Srikanth <srikantathikari@gmail.com>
2021-04-12 07:41:24 -0400

    Removed API Key

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const WEATHER_JOURNAL_APIKEY = process.env.WEATHER_JOURNAL_APIKEY
 const port = 8080;
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
-app.use(cors());
-
-app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static('public'))
+
+app.post('/weather', (req,res)=>{
+
+})
+
+
+// const cors = require('cors');
+
+// app.use(cors());
+
+// app.use(express.urlencoded({extended:true}));
 
 const server = app.listen(port, ()=>{console.log(`port number is :${port}`)});
 
 
-app.get('/', (req,res)=>{
-
-    res.send('We ve go you');
-
-})
