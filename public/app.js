76d7d988954390966549a9bd7b12db111d9e5b6c
Srikanth <srikantathikari@gmail.com>
2021-04-12 07:42:22 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 
 const baseURL = "http://home.openweathermap.org";
-const apiKey = "3b639a0eac455ca1b3814b30df7083d8";
 const inputFromUser = document.getElementById('zip').value;
 
 document.getElementById('generate').addEventListener('click', performAction);
